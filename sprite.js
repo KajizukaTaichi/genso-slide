9e91b38defe83b8685c3f8c6ba09fa1a629ea0fc
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-13 22:01:15 +0900

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -17,8 +17,8 @@ class Sprite {
         this.elm.style.left = fixLayoutX(this);
         this.elm.style.top = fixLayoutY(this);
 
-        this.elm.style.width = `${this.width}vw`;
-        this.elm.style.height = `${this.height}vh`;
+        this.elm.style.width = `${this.width}%`;
+        this.elm.style.height = `${this.height}%`;
 
         document.body.appendChild(this.elm);
     }
@@ -38,8 +38,8 @@ class Sprite {
     }
     size(width, height) {
         [this.width, this.height] = [width, height];
-        this.elm.style.width = `${width}vw`;
-        this.elm.style.height = `${height}vh`;
+        this.elm.style.width = `${width}%`;
+        this.elm.style.height = `${height}%`;
         this.position(this.x, this.y);
     }
     hide() {
@@ -55,7 +55,7 @@ function text(
     { size = 5, font = "Arial", color = "black", back = "white" } = {},
 ) {
     const elm = document.createElement("p");
-    elm.style.fontSize = `${size}vh`;
+    elm.style.fontSize = `${size}%`;
     elm.style.fontFamily = font;
     elm.style.color = color;
     elm.style.backgroundColor = back;
@@ -63,7 +63,7 @@ function text(
     elm.style.display = "flex";
     elm.style.alignItems = "center";
     elm.style.justifyContent = "center";
-    elm.style.padding = "1vh";
+    elm.style.padding = "1%";
     elm.style.margin = "0";
     elm.innerHTML = text;
     return elm;
@@ -71,12 +71,12 @@ function text(
 
 function image(url, { size = 5 } = {}) {
     const elm = document.createElement("img");
-    elm.style.height = `${size}vh`;
-    elm.style.padding = "1vh";
+    elm.style.height = `${size}%`;
+    elm.style.padding = "1%";
     elm.style.margin = "0";
     elm.src = url;
     return elm;
 }
 
-const fixLayoutX = (sprite) => `${sprite.x - sprite.width / 2}vw`;
-const fixLayoutY = (sprite) => `${sprite.y - sprite.height / 2}vh`;
+const fixLayoutX = (sprite) => `${sprite.x - sprite.width / 2}%`;
+const fixLayoutY = (sprite) => `${sprite.y - sprite.height / 2}%`;
