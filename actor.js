d5dde626d70c81339a0deacf96b230e9efbe224f
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-13 13:22:39 +0900

    Initial commit

---
+++ actor.js
@@ -0,0 +1,46 @@
+const Look = Object.freeze({ Normal: "normal", Say: "say" });
+const Position = Object.freeze({ Left: "left", Right: "right" });
+const Charactor = Object.freeze({ Reimu: "reimu", Marisa: "marisa" });
+
+class Actor {
+    constructor(name, position) {
+        // Set fields
+        [this.name, this.position] = [name, position];
+
+        // Element of the standing draw (立ち絵)
+        this.draw = document.createElement("img");
+        this.draw.src = image_url(this.name);
+
+        // Setting CSS
+        this.draw.style.position = "absolute";
+        this.draw.style.height = "50vh";
+        this.draw.style.zIndex = "999";
+        this.draw.style.bottom = "0";
+        this.draw.style[this.position] = "0";
+
+        // Show to HTML
+        document.body.appendChild(this.draw);
+    }
+
+    async say(audio_url) {
+        this.sayInterval = setInterval(() => {
+            const look = Object.values(Look)[Math.floor(Math.random() * 2)];
+            this.draw.src = image_url(this.name, look);
+        }, 100);
+        await waitPlayAudio(audio_url);
+        clearInterval(this.sayInterval);
+        this.draw.src = image_url(this.name, Look.say);
+    }
+}
+
+function waitPlayAudio(src) {
+    return new Promise((resolve, reject) => {
+        const audio = new Audio(src);
+        audio.addEventListener("ended", () => resolve());
+        audio.addEventListener("error", (e) => reject(e));
+        audio.play().catch(reject);
+    });
+}
+
+const image_url = (name, style = Look.Normal) =>
+    `../resource/${name}/${style}.jpg`;
---
+++ sprite.js
@@ -0,0 +1,52 @@
+let zIndex = 0;
+
+class Sprite {
+    constructor(elm) {
+        [this.x, this.y] = [0, 0];
+        this.width = this.height = "max-content";
+        this.set(elm);
+    }
+    set(elm) {
+        if (this.elm !== undefined) this.elm.remove();
+        this.elm = elm;
+
+        this.elm.style.position = "absolute";
+        this.elm.style.zIndex = zIndex++;
+
+        this.elm.style.left = this.x;
+        this.elm.style.top = this.y;
+
+        this.elm.style.width = this.width;
+        this.elm.style.height = this.height;
+
+        document.body.appendChild(this.elm);
+    }
+    position(x, y) {
+        [this.x, this.y] = [`${x}vw`, `${y}vh`];
+        this.elm.style.left = this.x;
+        this.elm.style.top = this.y;
+    }
+    size(x, y) {
+        [this.width, this.height] = [x, y];
+        this.elm.style.width = `${x}vw`;
+        this.elm.style.height = `${y}vh`;
+    }
+    hide() {
+        this.elm.style.display = "none";
+    }
+    show() {
+        this.elm.style.display = "block";
+    }
+    remove() {
+        this.elm.remove();
+    }
+}
+
+function text(text, size = 50) {
+    const elm = document.createElement("p");
+    elm.innerHTML = text;
+    elm.style.fontSize = `${size}px`;
+    elm.style.padding = "10px";
+    elm.style.margin = "0";
+    return elm;
+}
